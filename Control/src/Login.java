d56303afb69cd40cca96c52b4f7d152a91c56a86
Julio <julmar475@gmail.com>
2019-07-03 19:38:14 -0500

    Login

--- Control/src/Login.java
+++ Control/src/Login.java
@@ -6,9 +6,14 @@ import javax.swing.*;
 
 public class Login extends JFrame{
 	
+	JPanel panel1 = new JPanel();
+	
+	JTextField usuario = new JTextField();
+	JPasswordField password = new JPasswordField();
+	
 	public Login() {
 		this.setSize(400,600);   //establecemos tamaño
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE); //cerrar la ventana al precionar X
 		this.setTitle("Genesis");
 		this.setLocationRelativeTo(null); //establecemos posicion (centrado)
 		
@@ -16,14 +21,18 @@ public class Login extends JFrame{
 	}
 	
 	private void iniciarComponentes() {
-		JPanel panel1 = new JPanel();
-		panel1.setBackground(Color.BLACK);
+		
+		panel1.setBackground(Color.WHITE);
 		this.getContentPane().add(panel1);
+		panel1.setLayout(null);  // desactivando el diseño por defecto
 		colocarEtiquetas();
 	}
 	
 	public void colocarEtiquetas() {
-		JLabel titulo = new JLabel("GENESIS");
+		JLabel titulo = new JLabel("hi");
+		titulo.setBounds(100,100,200,100); //tamaño y posicion
+		titulo.setForeground(Color.black); //color de letra
+		panel1.add(titulo); //agregamos la etiqueta al panel
 	}
 
 }
